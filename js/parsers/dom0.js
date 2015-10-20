7329ba3a79d2d2580bf2eea815629d03b108f070
Yuhao Zhu <yuhao.ryan.zhu@gmail.com>
2015-10-20 15:59:24 -0500

    Upate QoSType within rAF, rather than in its callback. In this way, we don't have to care about any async push, etc.

--- js/parsers/dom0.js
+++ js/parsers/dom0.js
@@ -9,22 +9,22 @@
                   'unload' ],
         i, iLen, j, jLen = types.length;
 
-    function asyncPush(elements, node, type, func) {
-      // http://stackoverflow.com/questions/12959006/javascript-variables-undefined-within-settimeout-function
-      // settimeout doesn't pass arguments
-      setTimeout(function() {
-        elements.push({
-          "node": node,
-          "QoSType": QoSType,
-          "listeners": [{
-            "type": type,
-            "func": func,
-            "removed": false,
-            "source": 'DOM 0 event'
-          }]
-        });
-      }, 20);
-    }
+    //function asyncPush(elements, node, type, func) {
+    //  // http://stackoverflow.com/questions/12959006/javascript-variables-undefined-within-settimeout-function
+    //  // settimeout doesn't pass arguments
+    //  setTimeout(function() {
+    //    elements.push({
+    //      "node": node,
+    //      "QoSType": QoSType,
+    //      "listeners": [{
+    //        "type": type,
+    //        "func": func,
+    //        "removed": false,
+    //        "source": 'DOM 0 event'
+    //      }]
+    //    });
+    //  }, 20);
+    //}
 
     for ( i=0, iLen=all.length ; i<iLen ; i++ ) {
       for ( j=0 ; j<jLen ; j++ ) {
@@ -37,34 +37,34 @@
             QoSType = "single";
 
             // rAF
+            // Hijack rAF in case we want to do more later.
             var originalrAF = requestAnimationFrame;
             requestAnimationFrame = function(callback){
+              QoSType = "continuous";
+              QoSType += " :rAF";
               originalrAF(function(){
-                QoSType = "continuous";
-                //console.log("hijacked");
                 callback();
               });
             }
 
-            elem.click();
-            asyncPush(elements, all[i], types[j], all[i]['on'+types[j]].toString());
+            //asyncPush(elements, all[i], types[j], all[i]['on'+types[j]].toString());
 
             // CSS Transition
             var style = window.getComputedStyle(elem);
             if (style.getPropertyValue('transition') !== 'all 0s ease 0s') {
               QoSType = "continuous";
+              QoSType += " :CSSTransition";
               // TODO: trigger click, wait for |duration| second, and see if
               // the ontransitioned callback is executed.
               //elem.addEventListener("transitionend", function () {
               //  QoSType = "continuous";
               //}, true);
-              //elem.click();
             }
-
-            continue;
+            elem.click();
           }
           else if (types[j] == "scroll") {
             QoSType = "continuous";
+            QoSType += " :scroll";
           }
 
           elements.push({

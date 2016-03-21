2847872a6aa9d6a24f9e7ce518a7932ea89a947f
Yuhao Zhu <yuhao.ryan.zhu@gmail.com>
2016-03-21 16:25:19 -0500

    Update simple https server comments.

--- simple-https-server.py
+++ simple-https-server.py
@@ -1,10 +1,11 @@
+# Simple HTTPS server
 # taken from http://www.piware.de/2011/01/creating-an-https-server-in-python/
 # generate server.xml with the following command:
 #    openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
 # run as follows:
 #    python simple-https-server.py
 # then in your browser, visit:
-#    https://localhost:4443
+#    https://localhost:8000
 
 import BaseHTTPServer, SimpleHTTPServer
 import ssl

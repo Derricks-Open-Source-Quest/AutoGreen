c42e6664dcac0b2a016d357b9ae0eb390fe538ec
Yuhao Zhu <yuhao.ryan.zhu@gmail.com>
2016-03-21 15:43:54 -0500

    Clean up the builder script.

--- build.sh
+++ build.sh
@@ -2,40 +2,27 @@
 
 echo ""
 if [ "$1" = "-h" ]; then
-	echo "  Visual Event build script - usage:
-    ./build.sh [loader-dir] [debug]
-      loader-dir - The web-address of the build files. Note that the build
-        directory name is automatically appended. For example:
-          localhost/VisualEvent/builds - default if no option is provided
-          sprymedia.co.uk/VisualEvent/builds
-      debug - Debug indicator. Will not compress the Javascript
-
-    Example deploy build:
-      ./build.sh sprymedia.co.uk/VisualEvent/builds
-
-    Example debug build:
-      ./build.sh localhost/VisualEvent/builds debug
-"
-	exit
+  echo "  Visual Event build script - usage:
+    ./build.sh [mode]
+      mode: 'c' - compress the Javascript files
+            'd' - generate JSDoc
+            'cd' - composite of c and d"
+  exit
 elif [ "$1" = "clean" ]; then
-	echo "Cleaning Visual Event builds"
-	rm -Rf $(pwd)/builds/VisualEvent*
-	exit
+  echo "Cleaning Visual Event builds"
+  rm -Rf $(pwd)/out
+  exit
 fi
 
 # OPTIONS
-DEBUG=$1
+MODE=$1
 
-# DEFAULTS
-UGLIFYJS=/Users/allan/node_modules/uglify-js/bin/uglifyjs
-JSDOC3=/usr/local/jsdoc/jsdoc
+# Utilities
+UGLIFYJS=./node_modules/uglify-js/bin/uglifyjs
+JSDOC3=./node_modules/.bin/jsdoc
 
 BUILD_BASE=$(pwd)/out
-if [ "$DEBUG" = "debug" ]; then
-  BUILD_DIR=debug
-else
-  BUILD_DIR=release
-fi
+BUILD_DIR=release
 BUILD=$BUILD_BASE/${BUILD_DIR}
 BUILD_JS=${BUILD}/js
 BUILD_CSS=${BUILD}/css
@@ -50,26 +37,25 @@ echo "  Creating media directory ${BUILD}"
 rm -rf "$BUILD_BASE"
 mkdir -p "$BUILD"
 
-
 # JAVASCRIPT
 echo "  Javascript"
 mkdir "$BUILD_JS"
 
 echo "    Combining Javascript files"
 cp  "$JS/VisualEvent_Loader.js"                                               "$BUILD_BASE/VisualEvent_Loader.js"
-#cat "$JS/jquery.js" "$JS/shCore.js" "$JS/VisualEvent.js" "$JS"/parsers/*.js > "$BUILD_JS/VisualEvent-jQuery.js"
+# VisualEvent-jQuery.js for pages that do not already have jQuery. See the _construct function in js/VisualEvent_Loader.js.
 cat "$JS/jquery-1.10.2.js" "$JS/shCore.js" "$JS/VisualEvent.js" "$JS"/parsers/*.js > "$BUILD_JS/VisualEvent-jQuery.js"
-cat                 "$JS/shCore.js" "$JS/VisualEvent.js" "$JS"/parsers/*.js > "$BUILD_JS/VisualEvent.js"
+cat "$JS/shCore.js" "$JS/VisualEvent.js" "$JS"/parsers/*.js > "$BUILD_JS/VisualEvent.js"
 
-if [ "$DEBUG" != "debug" -a -e $UGLIFYJS ]; then
-	echo "    Compressing Javascript"
-	$UGLIFYJS $BUILD_BASE/VisualEvent_Loader.js > $BUILD_BASE/VisualEvent_Loader.min.js
-	$UGLIFYJS $BUILD_JS/VisualEvent-jQuery.js   > $BUILD_JS/VisualEvent-jQuery.min.js
-	$UGLIFYJS $BUILD_JS/VisualEvent.js          > $BUILD_JS/VisualEvent.min.js
-	
-	mv "$BUILD_BASE/VisualEvent_Loader.min.js" "$BUILD_BASE/VisualEvent_Loader.js"
-	mv "$BUILD_JS/VisualEvent-jQuery.min.js"   "$BUILD_JS/VisualEvent-jQuery.js"
-	mv "$BUILD_JS/VisualEvent.min.js"          "$BUILD_JS/VisualEvent.js"
+if [ "$MODE" == "c" -o "$MODE" == "cd" -a -e $UGLIFYJS ]; then
+  echo "    Compressing Javascript"
+  $UGLIFYJS $BUILD_BASE/VisualEvent_Loader.js > $BUILD_BASE/VisualEvent_Loader.min.js
+  $UGLIFYJS $BUILD_JS/VisualEvent-jQuery.js   > $BUILD_JS/VisualEvent-jQuery.min.js
+  $UGLIFYJS $BUILD_JS/VisualEvent.js          > $BUILD_JS/VisualEvent.min.js
+  
+  mv "$BUILD_BASE/VisualEvent_Loader.min.js" "$BUILD_BASE/VisualEvent_Loader.js"
+  mv "$BUILD_JS/VisualEvent-jQuery.min.js"   "$BUILD_JS/VisualEvent-jQuery.js"
+  mv "$BUILD_JS/VisualEvent.min.js"          "$BUILD_JS/VisualEvent.js"
 fi
 
 # This is very obscure..
@@ -87,11 +73,11 @@ cat "$CSS/VisualEvent.css" "$CSS/shCore.css" > "$BUILD_CSS/VisualEvent.css"
 
 
 # Docs
-if [ -e $JSDOC3 -a "$DEBUG" != "debug" ]; then
-	echo "  Documentation"
-	$JSDOC3 -d $BUILD_DOCS -t JSDoc-DataTables $JS/VisualEvent.js $JS/VisualEvent_Loader.js
+if [ -e $JSDOC3 -a "$MODE" == "d" -o "$MODE" == "cd" ]; then
+  echo "  Generating JSDoc"
+  $JSDOC3 -d $BUILD_DOCS $JS/VisualEvent.js $JS/VisualEvent_Loader.js
 fi
 
-echo "Done :-)"
+echo "Done"
 echo ""
 
